51737eab6647accabb59358138e99bca5297775d
Pedro Henrique <161378527+Pedro0Silvestre@users.noreply.github.com>
2025-10-08 22:31:33 -0300

    Create deteccao_simples.py

---
+++ deteccao_simples.py
@@ -0,0 +1,49 @@
+# deteccao_simples.py
+
+#essa versão foi a primeira implementada contém o modelo COCO do YOLOV8 que identifica objetos genéricos. Para o projeto final usaremos uma versão desse modelo com fine-tuning
+
+import cv2
+from ultralytics import YOLO
+
+# --- CONFIGURAÇÃO DO MODELO ---
+
+try:
+    model = YOLO('yolov8n.pt')
+    print("Modelo YOLOv8 carregado com sucesso!")
+except Exception as e:
+    print(f"Erro ao carregar o modelo: {e}")
+    exit()
+
+
+# Abre a webcam
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Erro: Não foi possível abrir a webcam.")
+    exit()
+
+print("Webcam iniciada. Pressione 'q' para sair.")
+
+while True:
+    
+    ret, frame = cap.read()
+    if not ret:
+        print("Fim do stream de vídeo ou erro de captura.")
+        break
+
+    
+    results = model(frame, verbose=False) # verbose=False para não poluir o console
+
+    # .plot() adiciona as detecções ao frame
+    annotated_frame = results[0].plot()
+
+    # Exibe o frame com as detecções em uma janela
+    cv2.imshow("Detecção de Objetos YOLOv8", annotated_frame)
+
+    # Encerra detecção caso apertar 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Libera os recursos ao finalizar
+cap.release()
+cv2.destroyAllWindows()
+print("Aplicação encerrada.")
